eae50a40375c477640dd8322a075d99bad484798
fanyixiong <fanyixiong@vip.qq.com>
2023-08-09 21:29:17 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -101,7 +101,7 @@ export default {
                     default:
                         // return new Response('Not found', { status: 404 });
                         // For any other path, reverse proxy to 'maimai.sega.jp' and return the original response
-                        url.hostname = 'www.baidu.com';
+                        url.hostname = 'maimai.sega.jp';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
